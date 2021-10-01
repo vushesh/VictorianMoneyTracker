4ed77afeb145a9cf8c92774c2ca145f30cc70421
VusheshDookheea <69453459+VusheshDookheea@users.noreply.github.com>
2021-10-01 13:33:33 +0800

    Initial commit

---
+++ VictorianMoneyTracker/VictorianMoneyTracker/DataModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Linq;
+using Xamarin.Forms.Xaml;
+using VictorianMoneyTracker;
+using System.Windows.Input;
+using Xamarin.Forms.Internals;
+
+namespace VictorianMoneyTracker
+{
+    public partial class DataModel
+    {
+
+        //--------Conversions Rate ---------
+        int PoundToCrown = 4;
+        int CrownToShillings = 5;
+        int ShillingsToPence = 12;
+
+        //--------------------------------
+
+
+        public int Pound { get; set; }
+        public int Crown { get; set; }
+        public int Shillings { get; set; }
+        public int Pence { get; set; }
+        public int Farthings { get; set; }
+        
+        //-----------Pound Conversions------------
+
+        public void AddPound()
+        {
+            Pound++;
+        }
+        public void MinusPound()
+        {
+            Pound--;
+        }
+        public void ConvertPoundIncrease()
+        {
+            //doesnt exist
+        }
+        public void ConvertPoundDecrease()
+        {
+            if(Pound >= 1)
+            {
+                Pound--;
+                Crown += PoundToCrown;
+            }
+            
+        }
+
+
+        //-----------Crowns Conversions------------
+
+        public void AddCrown()
+        {
+            Crown++;
+        }
+        public void MinusCrown()
+        {
+            Crown--;
+        }
+        public void ConvertCrownIncrease()
+        {
+            if (Crown >= PoundToCrown)
+            {
+                Crown -= PoundToCrown;
+                Pound++;
+            }
+        }
+        public void ConvertCrownDecrease()
+        {
+            if (Crown >= 1)
+            {
+                Crown--;
+                Shillings += CrownToShillings;
+            }
+        }
+
+        //--------Shillings Conversions---------
+        
+        public void AddShillings()
+        {
+            Shillings++;
+        }
+        public void MinusShillings()
+        {
+            Shillings--;
+        }
+        public void ConvertShillingsIncrease()
+        {
+            if (Shillings >= CrownToShillings)
+            {
+                Shillings -= CrownToShillings;
+                Crown++;
+            }
+        }
+        public void ConvertShllingsDecrease()
+        {
+            if (Shillings >= 1)
+            {
+                Shillings--;
+                Pence += ShillingsToPence;
+            }
+        }
+
+
+        //---------------------------
+
+
+
+
+        public void AddPence()
+        {
+            Pence++;
+        }
+        public void MinusPence()
+        {
+            Pence--;
+        }
+
+
+
+        public void AddFarthings()
+        {
+            Farthings++;
+        }
+        public void MinusFarthings()
+        {
+            Farthings--;
+        }
+    }
+}
---
+++ VictorianMoneyTracker/VictorianMoneyTracker/MainPage.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VictorianMoneyTracker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)] 
+    public partial class MainPage : ContentPage
+    {
+        DataModel model = new DataModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+
+        //---------- Increase Count on Text---------
+        private void AddCrownsClicked(object sender, EventArgs e)
+        {
+            model.AddCrown();
+            UpdateUI();
+        }
+
+
+        private void UpdateUI()
+        {
+            PoundLabel.Text = model.Pound.ToString();
+            totalPound.Text = model.Pound.ToString();
+            
+            crownLabel.Text = model.Crown.ToString();
+            totalCrown.Text = model.Crown.ToString();
+            
+            shillingLabel.Text = model.Shillings.ToString();
+            totalShillings.Text = string.Format(model.Shillings.ToString(), "S");
+            
+            penceLabel.Text = model.Pence.ToString();
+            totalPence.Text = string.Format(model.Pence.ToString(), "d");
+
+        }
+
+        private void AddPoundClicked(object sender, EventArgs e)
+        {
+            model.AddPound();
+            UpdateUI();
+        }
+
+
+        private void AddshillingsClicked(object sender, EventArgs e)
+        {
+            model.AddShillings();
+            UpdateUI();
+        }
+
+
+        private void AddpenceClicked(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void AddfarthingsClicked(object sender, EventArgs e)
+        {
+            
+
+        }
+
+
+        // ----------Decrease Count For Text-----------
+        private void minusPound(object sender, EventArgs e)
+        {
+            model.MinusPound();
+            UpdateUI();
+        }
+
+        private void minusCrowns(object sender, EventArgs e)
+        {
+            model.MinusCrown();
+            UpdateUI();
+        }
+
+        private void minusShillings(object sender, EventArgs e)
+        {
+            model.MinusShillings();
+            UpdateUI();
+        }
+
+        private void minusPence(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void minusFarthing(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        // -----------Conversions Buttons----------
+        private void crownExchanged(object sender, EventArgs e)
+        {
+            model.ConvertCrownIncrease();
+            UpdateUI();
+        }
+
+        
+        
+        private void poundExchanged(object sender, EventArgs e)
+        {
+            model.ConvertPoundIncrease();
+            UpdateUI();
+        }
+
+        
+        
+        private void shillingsExchanged(object sender, EventArgs e)
+        {
+            model.ConvertShillingsIncrease();
+            UpdateUI();
+        }
+
+        
+        
+        private void poundExchangedDec(object sender, EventArgs e)
+        {
+
+            model.ConvertPoundDecrease();
+            UpdateUI();
+        }
+
+        
+        
+        private void crownExchangedDec(object sender, EventArgs e)
+        {
+            model.ConvertCrownDecrease();
+            UpdateUI();
+        }
+
+        
+        
+        private void shillingsExchangedDec(object sender, EventArgs e)
+        {
+            model.ConvertShllingsDecrease();
+            UpdateUI();
+        }
+    }
+}
